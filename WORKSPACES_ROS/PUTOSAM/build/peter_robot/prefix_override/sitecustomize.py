90f28553f13cf934ba61aca4b9b92c4159c0bb04
Diego Caro <147681608+DiegoACaro@users.noreply.github.com>
2025-02-23 18:07:41 -0500

    commmit sin log,build

--- WORKSPACES_ROS/PUTOSAM/build/peter_robot/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/alejo/Paper/PETER_SIMULATION/WORKSPACES_ROS/PUTOSAM/install/peter_robot'
