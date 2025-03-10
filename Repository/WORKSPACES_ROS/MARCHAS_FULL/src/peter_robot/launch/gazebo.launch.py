8c58e569359fa73adc6f24504c9fea005cc7303f
Diego Caro <147681608+DiegoACaro@users.noreply.github.com>
2025-03-10 14:59:52 -0500

    CarpetaDocker

---
+++ Repository/WORKSPACES_ROS/MARCHAS_FULL/src/peter_robot/launch/gazebo.launch.py
@@ -0,0 +1,98 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import xacro
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    package_name = 'peter_robot'
+    pkg_share = FindPackageShare(package=package_name).find(package_name)
+    
+    world_file_path = 'empty.world'
+    world_path = os.path.join(pkg_share, 'worlds',  world_file_path)
+
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        name='use_sim_time',
+        default_value='true',
+        description='Use simulation (Gazebo) clock if true'
+    )
+
+    pkg_path = os.path.join(get_package_share_directory('peter_robot'))
+    xacro_file = os.path.join(pkg_path,'urdf','peter.urdf.xacro')
+    robot_description_config = xacro.process_file(xacro_file)
+
+    params = {'robot_description': robot_description_config.toxml()}
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[params]
+    )
+
+    start_joint_state_publisher_cmd = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        parameters=[{'use_sim_time': use_sim_time}],
+        name='joint_state_publisher'
+    )
+
+    peter_controller = Node(
+        package='peter_robot',
+        executable='peter_controller',
+        name='peter_controller',
+        output='screen'
+    )
+
+    load_joint_state_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+            'joint_state_broadcaster'],
+        output='screen' 
+    )
+
+    load_forward_command_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active', 
+            'gazebo_joint_controller'],
+        output='screen'
+    )
+
+    # Cargar el controlador de velocidad para las ruedas
+    load_velocity_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'gazebo_velocity_controllers'],
+        output='screen'
+    )
+
+    spawn = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=["-topic", "/robot_description", 
+                   "-entity", 'peter_urdf',
+                   "-x", '0.0',
+                   "-y", '0.0',
+                   "-z", '0.8',
+                   "-Y", '0.0'],
+        output='screen'
+    )
+
+    gazebo = ExecuteProcess(
+        cmd=['gazebo', world_path, '--verbose', '-s', 'libgazebo_ros_factory.so', '-s', 'libgazebo_ros_init.so'],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        declare_use_sim_time_cmd,
+        node_robot_state_publisher,
+        gazebo,
+        spawn,
+        start_joint_state_publisher_cmd, 
+        load_joint_state_controller,
+        load_forward_command_controller,
+        load_velocity_controller,
+        peter_controller
+        
+    ])
---
+++ Repository/WORKSPACES_ROS/PUTOSAM/src/camera/src/camerav2.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from std_msgs.msg import Int32MultiArray, Float32MultiArray
+from cv_bridge import CvBridge
+import cv2
+import numpy as np
+
+class CamaraNodo(Node):
+    def __init__(self):
+        super().__init__('camera_viewer')
+        self.bridge = CvBridge()
+        
+        self.camera_subscription = self.create_subscription(
+            Image,
+            '/camera_sensor/image_raw',  # Tópico de la cámara
+            self.listener_callback,
+            10
+        )
+
+        # Publicadores para cada color
+        self.publisher_red = self.create_publisher(Float32MultiArray, '/bounding_box/red', 10)
+        self.publisher_blue = self.create_publisher(Float32MultiArray, '/bounding_box/blue', 10)
+        self.publisher_green = self.create_publisher(Float32MultiArray, '/bounding_box/green', 10)
+
+        self.get_logger().info("Nodo de visualización de cámara iniciado.")
+
+    def publish_bounding(self, publisher, x, y, w, h, colorname):
+        msg = Float32MultiArray()
+        x_mapped = float(180 - (x / 639) * 180) #Posicion de 0 a 180 para la red SI NECESITA VALOR ENTERO CAMBIAR
+        area = float(w*h)
+        msg.data = [x_mapped, area]
+        self.get_logger().info(f'Array {colorname}: {[x, y, w, h]}')
+        publisher.publish(msg)
+
+    def remove_small_objects(self, image, min_size):
+        num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(image, connectivity=8)
+        for label in range(1, num_labels):
+            if stats[label, cv2.CC_STAT_AREA] < min_size:
+                labels[labels == label] = 0
+        return np.uint8(labels > 0) * 255
+
+    def filter_and_draw(self, frame, hsv, lower_color, upper_color, color_bgr, publisher, colorname):
+        mask = cv2.inRange(hsv, lower_color, upper_color)
+        mask = self.remove_small_objects(mask, min_size=700)
+
+        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        for contour in contours:
+            x, y, w, h = cv2.boundingRect(contour)
+            area = cv2.contourArea(contour)
+            if area > 100:
+                cv2.rectangle(frame, (x, y), (x + w, y + h), color_bgr, 2)
+                self.publish_bounding(publisher, x, y, w, h, colorname)
+
+    def listener_callback(self, msg):
+        frame = self.bridge.imgmsg_to_cv2(msg, 'bgr8')
+        frame = cv2.resize(frame, (640, 480), interpolation=cv2.INTER_CUBIC)
+
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        # Filtros de color (HSV)
+        color_filters = {
+            'red': ((0, 100, 100), (10, 255, 255)),
+            'blue': ((80, 0, 150), (255, 255, 255)),
+            'green': ((5, 80, 80), (80, 255, 255)),
+        }
+
+        # Detectar y dibujar las bounding boxes
+        self.filter_and_draw(frame, hsv, *color_filters['red'], (0, 0, 255), self.publisher_red, "Red")
+        self.filter_and_draw(frame, hsv, *color_filters['blue'], (255, 0, 0), self.publisher_blue, "Blue")
+        self.filter_and_draw(frame, hsv, *color_filters['green'], (0, 255, 0), self.publisher_green, "Green")
+
+        cv2.imshow('Video', frame)
+        cv2.waitKey(1)
+
+def main(args=None):
+    rclpy.init(args=args)
+    viewer = CamaraNodo()
+    rclpy.spin(viewer)
+    viewer.destroy_node()
+    rclpy.shutdown()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
